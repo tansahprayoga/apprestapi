eeda534787a586871f75ba296c0d50cf7919f7af
tansah prayoga <prayogatansah@gmail.com>
2020-04-13 19:40:42 +0700

    berhasil menampilkan data semua mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("APLIKASI REST API BERJALAN")
+    response.ok("APLIKASI REST API BERJALAN",res)
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error, rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
 };
