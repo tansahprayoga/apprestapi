2a33da4782d830f9e335084738b8ea782aee7042
tansah prayoga <prayogatansah@gmail.com>
2020-04-20 09:52:01 +0700

    registrasi berhasil

--- middleware/index.js
+++ middleware/index.js
@@ -3,6 +3,6 @@ var auth = require('./auth');
 var router = express.Router();
 
 //daftarkan menu registrasi
-router.post('/api/v1/register',auth,registrasi);
+router.post('/api/v1/register',auth.registrasi);
 
 module.exports = router;
